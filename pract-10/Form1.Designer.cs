5d3ae2b074d5668d8eca44a27b0dbc6326cbc561
Kote\Kote_Animen <sverh.il@yandex.ru>
2020-11-19 18:39:57 +0300

    Добавьте файлы проекта.

---
+++ pract-10/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace pract_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.AboutUs = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.productsList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.answer = new System.Windows.Forms.TextBox();
+            this.GetAnswer = new System.Windows.Forms.Button();
+            this.nameProduct = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selfCost = new System.Windows.Forms.NumericUpDown();
+            this.costSell = new System.Windows.Forms.NumericUpDown();
+            this.AddToList = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.selfCost)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.costSell)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutUs,
+            this.Exit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(219, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // AboutUs
+            // 
+            this.AboutUs.Name = "AboutUs";
+            this.AboutUs.Size = new System.Drawing.Size(94, 20);
+            this.AboutUs.Text = "О программе";
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(53, 20);
+            this.Exit.Text = "Выход";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(194, 264);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.productsList);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(186, 238);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Список продуктов";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.AddToList);
+            this.tabPage2.Controls.Add(this.costSell);
+            this.tabPage2.Controls.Add(this.selfCost);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.nameProduct);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(186, 238);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Добавить";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // productsList
+            // 
+            this.productsList.FormattingEnabled = true;
+            this.productsList.Location = new System.Drawing.Point(7, 7);
+            this.productsList.Name = "productsList";
+            this.productsList.Size = new System.Drawing.Size(173, 121);
+            this.productsList.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.GetAnswer);
+            this.groupBox1.Controls.Add(this.answer);
+            this.groupBox1.Location = new System.Drawing.Point(7, 140);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 92);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Номер продукта";
+            // 
+            // answer
+            // 
+            this.answer.Location = new System.Drawing.Point(7, 19);
+            this.answer.Name = "answer";
+            this.answer.ReadOnly = true;
+            this.answer.Size = new System.Drawing.Size(160, 20);
+            this.answer.TabIndex = 0;
+            // 
+            // GetAnswer
+            // 
+            this.GetAnswer.Location = new System.Drawing.Point(7, 55);
+            this.GetAnswer.Name = "GetAnswer";
+            this.GetAnswer.Size = new System.Drawing.Size(160, 27);
+            this.GetAnswer.TabIndex = 1;
+            this.GetAnswer.Text = "Получить ответ";
+            this.GetAnswer.UseVisualStyleBackColor = true;
+            // 
+            // nameProduct
+            // 
+            this.nameProduct.Location = new System.Drawing.Point(7, 32);
+            this.nameProduct.Name = "nameProduct";
+            this.nameProduct.Size = new System.Drawing.Size(173, 20);
+            this.nameProduct.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название продукта";
+            // 
+            // selfCost
+            // 
+            this.selfCost.Location = new System.Drawing.Point(7, 75);
+            this.selfCost.Name = "selfCost";
+            this.selfCost.Size = new System.Drawing.Size(173, 20);
+            this.selfCost.TabIndex = 2;
+            // 
+            // costSell
+            // 
+            this.costSell.Location = new System.Drawing.Point(7, 123);
+            this.costSell.Name = "costSell";
+            this.costSell.Size = new System.Drawing.Size(173, 20);
+            this.costSell.TabIndex = 3;
+            // 
+            // AddToList
+            // 
+            this.AddToList.Location = new System.Drawing.Point(7, 176);
+            this.AddToList.Name = "AddToList";
+            this.AddToList.Size = new System.Drawing.Size(173, 23);
+            this.AddToList.TabIndex = 4;
+            this.AddToList.Text = "Добавить в список";
+            this.AddToList.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Затраты на производство";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Цена на продукт";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 304);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Практическая №10";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.selfCost)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.costSell)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem AboutUs;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button GetAnswer;
+        private System.Windows.Forms.TextBox answer;
+        private System.Windows.Forms.ListBox productsList;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AddToList;
+        private System.Windows.Forms.NumericUpDown costSell;
+        private System.Windows.Forms.NumericUpDown selfCost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameProduct;
+    }
+}
+
