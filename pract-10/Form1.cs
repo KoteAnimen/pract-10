d02a173da8e10a4c16114248845e6a8a879e4fa7
ИСП-31 <-31@LAB24-01>
2020-11-20 09:50:47 +0300

    Задание готово, только как мне нравится

--- pract-10/Form1.cs
+++ pract-10/Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace pract_10
 {
+
+    public struct Product
+    {
+        public int selfCost;
+        public int costSell;
+
+        public Product(int self_Cost, int cost_Sell)
+        {
+            selfCost = self_Cost;
+            costSell = cost_Sell;
+        }
+    }
+
     public partial class Form1 : Form
     {
+        public List<Product> products = new List<Product>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GetAnswer_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while(i < products.Count)
+            {
+                if(products[i].costSell - products[i].selfCost < 0)
+                {
+                    answer.Text = (i + 1).ToString();
+                    break;                    
+                }
+                i++;
+            }
+        }
+
+        private void AddToList_Click(object sender, EventArgs e)
+        {
+            Product product = new Product((int)selfCost.Value, (int)costSell.Value);
+            products.Add(product);
+            productsList.Items.Add(products.Count + ". " + nameProduct.Text + " затраты - " + product.selfCost + ", цена - " + product.costSell);
         }
     }
 }
